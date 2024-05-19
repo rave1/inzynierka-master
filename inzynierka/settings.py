76256027a0b6fea08743ea921d200f0e12618908
Sam Sepiol <jakubsopel7@gmail.com>
2024-05-19 17:53:12 +0200

    chore: pre-commit, ruff

--- inzynierka/settings.py
+++ inzynierka/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = "inzynierka.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'mydatabase'),
-        'USER': os.getenv('DB_USER', 'myuser'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'mypassword'),
-        'HOST': os.getenv('DB_HOST', 'db'),
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME", "mydatabase"),
+        "USER": os.getenv("DB_USER", "myuser"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "mypassword"),
+        "HOST": os.getenv("DB_HOST", "db"),
+        "PORT": "3306",
     }
 }
 
